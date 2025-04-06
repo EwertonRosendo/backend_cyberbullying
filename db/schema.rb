616e834fecba1bbd31db35df1bb686e2d2f2a28b
Rosendo <93278917+EwertonRosendo@users.noreply.github.com>
2025-04-05 23:33:06 -0300

    create user model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_06_022039) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_06_023118) do
   create_table "tickets", force: :cascade do |t|
     t.string "email"
     t.text "ticket_description"
@@ -18,5 +18,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_06_022039) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "social_midia"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
