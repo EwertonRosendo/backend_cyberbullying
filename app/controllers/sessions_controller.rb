39072410d98a54ca657e89ba456660e6e73e831f
Rosendo <93278917+EwertonRosendo@users.noreply.github.com>
2025-05-14 02:56:37 +0000

    set up sessions to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 class SessionsController < ApplicationController
+  before_action :set_user, only: %i[ create ]
   def create
-    if user&.authenticate(params[:password])
-      render json: { message: "Logged in successfully", user: user }
+    if @user&.authenticate(user_params[:password])
+      render json: { message: "Logged in successfully", user: @user }
     else
       render json: { error: "Invalid email or password" }, status: :unauthorized
     end
@@ -11,4 +12,13 @@ class SessionsController < ApplicationController
     # You can handle logout here (e.g., clear token or session)
     render json: { message: "Logged out" }
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_digest, :photo, :kind)
+    end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find_by(email: user_params[:email])
+    end
 end
